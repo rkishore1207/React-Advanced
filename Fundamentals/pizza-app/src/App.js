124a9f049ba04e2c451db79cf74707484b913d3a
Kishore <146698138+rkishore1207@users.noreply.github.com>
2024-01-21 07:45:58 +0000

    learned conditional rendering and completed pizza project

--- Fundamentals/pizza-app/src/App.js
+++ Fundamentals/pizza-app/src/App.js
@@ -1,53 +1,9 @@
-const App = () => {
+import './index.css';
 
-    const pizzaData = [
-        {
-          name: "Focaccia",
-          ingredients: "Bread with italian olive oil and rosemary",
-          price: 6,
-          photoName: "pizzas/focaccia.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Margherita",
-          ingredients: "Tomato and mozarella",
-          price: 10,
-          photoName: "pizzas/margherita.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Spinaci",
-          ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
-          price: 12,
-          photoName: "pizzas/spinaci.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Funghi",
-          ingredients: "Tomato, mozarella, mushrooms, and onion",
-          price: 12,
-          photoName: "pizzas/funghi.jpg",
-          soldOut: false,
-        },
-        {
-          name: "Pizza Salamino",
-          ingredients: "Tomato, mozarella, and pepperoni",
-          price: 15,
-          photoName: "pizzas/salamino.jpg",
-          soldOut: true,
-        },
-        {
-          name: "Pizza Prosciutto",
-          ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
-          price: 18,
-          photoName: "pizzas/prosciutto.jpg",
-          soldOut: false,
-        },
-      ];
-      
+const App = () => {  
 
     return(
-        <div>
+        <div className = 'container'>
             <Header/>
             <Menu/>
             <Footer/>
@@ -59,20 +15,86 @@ export default App;
 
 const Header = () => {
   return(
-    <div>
+    <header className = "header">
       <h1>Fast React Pizza Co.</h1>
-    </div>
+    </header>
   )
 }
 
 const Menu = () => {
+
+  const pizzaData = [
+    {
+      name: "Focaccia",
+      ingredients: "Bread with italian olive oil and rosemary",
+      price: 6,
+      photoName: "pizzas/focaccia.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Margherita",
+      ingredients: "Tomato and mozarella",
+      price: 10,
+      photoName: "pizzas/margherita.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Spinaci",
+      ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+      price: 12,
+      photoName: "pizzas/spinaci.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Funghi",
+      ingredients: "Tomato, mozarella, mushrooms, and onion",
+      price: 12,
+      photoName: "pizzas/funghi.jpg",
+      soldOut: false,
+    },
+    {
+      name: "Pizza Salamino",
+      ingredients: "Tomato, mozarella, and pepperoni",
+      price: 15,
+      photoName: "pizzas/salamino.jpg",
+      soldOut: true,
+    },
+    {
+      name: "Pizza Prosciutto",
+      ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+      price: 18,
+      photoName: "pizzas/prosciutto.jpg",
+      soldOut: false,
+    },
+  ];
+
   return(
-    <div>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
-      <Pizza/>
+    <div className = 'menu'>
+      <ul className='pizzas'>
+        {
+          pizzaData.length > 0 && 
+          pizzaData.map((pizza,index)=>{
+            return(
+              <Pizza pizzaObj = {pizza} key={index}/>
+              )
+            })
+        }
+      </ul>
     </div>
+  )
+}
+
+const Pizza = (props) => {
+
+  if(props.pizzaObj.soldOut) return null;
+
+  return(
+      <li className = 'pizza'>
+        <h2>{props.pizzaObj.name}</h2>
+        <img src = {props.pizzaObj.photoName} alt = {props.pizzaObj.name}/>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </li>
   )
 }
 
@@ -86,17 +108,28 @@ const Footer = () => {
 
   return(
     <div>
-      <footer> {new Date().toLocaleTimeString()} We are currently Open</footer>
+      <footer className = 'footer'> 
+      {
+        isOpen ? (
+          <Order closeHour={closeHour}/>
+        ) : (
+          <div>
+            <p>We are Open only between {openHour} : 00 and {closeHour} : 00</p>
+          </div>
+        )
+      }
+      </footer>
     </div>
   )
 }
 
-const Pizza = () => {
-    return(
-        <div>
-            <h3>Pizza Prosciutto</h3>
-            <img src = "pizzas/prosciutto.jpg" alt = "prosciutto"/>
-            <p>Tomato, mozarella, ham, aragula, and burrata cheese</p>
-        </div>
-    )
+const Order = (props) => {
+  return(
+    <div className='order'>
+      <p>We are open till {props.closeHour} : 00</p>
+      <div>
+        <button className='btn'>Order</button>
+      </div>
+    </div>
+  )
 }
